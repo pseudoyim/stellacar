942340bfb36bb8c3accba5a3f72fd8fab10379f6
pseudoyim <paul.j.yim@gmail.com>
2017-09-17 13:12:26 -0500

    Updated drive.py

--- drive.py
+++ drive.py
@@ -1,12 +1,13 @@
 '''
 Allows user to input (via CLI) a series of commands to have the car execute when run.
 '''
-import stellacar
+from stellacar import wheel
 
 
 def commands():
 
 	series = []
+	series_for_wheels_to_execute = []
 
 	counter = 1
 
@@ -38,14 +39,44 @@ def commands():
 		elif done == 'n':
 			accepting_commands = False
 
-			print series
-			return series
+			print 'Here are the commands you\'ve asked me to perform: ', series
+
+		# Translate series for wheel function.
+		for pair in series:
+			translated_pair = translate_commands_to_wheel(pair)
+			series_for_wheels_to_execute.append(translated_pair)
+
+		# Execute translated commands.
+		for each in series_for_wheels_to_execute:
+			wheel(each)
+
+		print 'All done!'
+
+
+
+def translate_commands_to_wheel(pair)
+
+	if pair[0] == f:
+		wheels_dir = (lf,rf)
+
+	elif pair[0] == b:
+		wheels_dir = (lr,rr)
+
+	elif pair[0] == r:
+		wheels_dir = (lf, rr)
+
+	elif pair[0] == l:
+		wheels_dir = (lr, rf)
+
+	# Return the tuble of (wheels & dirs) and the duration.
+	return wheels_dir, pair[1]
+
 
 
 if __name__ == '__main__':
 	
 	print ''
 	print '****************************** Hi, Stella! *****************************'
-	print ''
+	print 'Please enter the list of commands you would like me to perform.'
 
 	commands()
